9d8fa43c6ffecce351c58c8a3b65cb68c6a1e128
Victor Rodrigues <victorrsilva17@gmail.com>
2021-09-18 19:59:04 -0300

    #3 Adicionando Velocimetro

--- ColetaFacil/src/Pages/Dashboard/Vehicle/index.tsx
+++ ColetaFacil/src/Pages/Dashboard/Vehicle/index.tsx
@@ -1,13 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text} from 'react-native';
 import {Container} from './styles';
+import Speedometer from 'react-native-speedometer-chart';
+import RNSpeedometer from 'react-native-speedometer';
 
 const Vehicle: React.FC = () => {
   return (
     <Container>
-      <Text>Carroça</Text>
+      <RNSpeedometer
+        value={40}
+        size={300}
+        maxValue={50}
+        labels={speedometerLabels}
+      />
     </Container>
   );
 };
+
+const speedometerLabels = [
+  {
+    name: 'Too Slow',
+    labelColor: '#c8b7b3',
+    activeBarColor: '#646362',
+  },
+  {
+    name: 'Very Slow',
+    labelColor: '#ff5400',
+    activeBarColor: '#ff5400',
+  },
+  {
+    name: 'Slow',
+    labelColor: '#f4ab44',
+    activeBarColor: '#f4ab44',
+  },
+  {
+    name: 'Normal',
+    labelColor: '#f2cf1f',
+    activeBarColor: '#f2cf1f',
+  },
+  {
+    name: 'Fast',
+    labelColor: '#14eb6e',
+    activeBarColor: '#14eb6e',
+  },
+  {
+    name: 'Unbelievably Fast',
+    labelColor: '#00ff6b',
+    activeBarColor: '#00ff6b',
+  },
+];
 
 export default Vehicle;
