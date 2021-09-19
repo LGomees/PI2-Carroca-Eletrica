db5aa744f5858a758699ac001d734063888b5070
Victor Rodrigues <victorrsilva17@gmail.com>
2021-09-19 20:45:03 -0300

    #3 Adicionando Setas, Nível de Bateria e Peso
    Co-Authored-By: Miguel Alves <miguelhenriquealvesdeoliveira@gmail.com>

--- ColetaFacil/src/Pages/Dashboard/Vehicle/index.tsx
+++ ColetaFacil/src/Pages/Dashboard/Vehicle/index.tsx
@@ -1,52 +1,93 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text} from 'react-native';
-import {Container} from './styles';
+import {
+  BatteryContainer,
+  BlinkersContainer,
+  Container,
+  InfosContainer,
+  SpeedometerContainer,
+  WeightContainer,
+} from './styles';
 import Speedometer from 'react-native-speedometer-chart';
 import RNSpeedometer from 'react-native-speedometer';
+import ProgressCircle from 'react-native-progress/Circle';
+import {Icon} from 'react-native-elements';
+
+const blinkersColor = ['#d2d3d5', '#3aafb9'];
+const speeds = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+  23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 34, 35, 36, 37, 38, 39, 40,
+  41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+];
 
 const Vehicle: React.FC = () => {
+  const [color, setColor] = useState(0);
+  const [speed, setSpeed] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setColor(v => (v === 1 ? 0 : v + 1));
+      setSpeed(s => (s === 49 ? 0 : s + 1));
+    }, 1500);
+  }, []);
+
   return (
     <Container>
-      <RNSpeedometer
-        value={40}
-        size={300}
-        maxValue={50}
-        labels={speedometerLabels}
-      />
+      <SpeedometerContainer>
+        <BlinkersContainer>
+          <Icon
+            name="arrow-left"
+            type="font-awesome"
+            color={blinkersColor[color]}
+            size={40}
+            onPress={() => console.log('hello')}
+          />
+          <Icon
+            name="arrow-right"
+            type="font-awesome"
+            color={blinkersColor[color]}
+            size={40}
+            onPress={() => console.log('hello')}
+          />
+        </BlinkersContainer>
+        <RNSpeedometer
+          value={speeds[speed]}
+          size={300}
+          maxValue={50}
+          labels={speedometerLabels}
+        />
+      </SpeedometerContainer>
+      <InfosContainer>
+        <BatteryContainer>
+          <Text>Nível da Bateria</Text>
+          <ProgressCircle
+            progress={0.5}
+            size={100}
+            animated
+            thickness={10}
+            showsText
+            color="#3aafb9"
+          />
+        </BatteryContainer>
+        <WeightContainer>
+          <Text>250</Text>
+          <Icon
+            name="weight-kilogram"
+            type="material-community"
+            size={70}
+            color="#093a3e"
+          />
+        </WeightContainer>
+      </InfosContainer>
     </Container>
   );
 };
 
 const speedometerLabels = [
   {
-    name: 'Too Slow',
-    labelColor: '#c8b7b3',
+    name: 'Km/h',
+    labelColor: '#3aafb9',
     activeBarColor: '#646362',
-  },
-  {
-    name: 'Very Slow',
-    labelColor: '#ff5400',
-    activeBarColor: '#ff5400',
-  },
-  {
-    name: 'Slow',
-    labelColor: '#f4ab44',
-    activeBarColor: '#f4ab44',
-  },
-  {
-    name: 'Normal',
-    labelColor: '#f2cf1f',
-    activeBarColor: '#f2cf1f',
-  },
-  {
-    name: 'Fast',
-    labelColor: '#14eb6e',
-    activeBarColor: '#14eb6e',
-  },
-  {
-    name: 'Unbelievably Fast',
-    labelColor: '#00ff6b',
-    activeBarColor: '#00ff6b',
   },
 ];
 
--- ColetaFacil/src/Pages/Dashboard/Vehicle/styles.ts
+++ ColetaFacil/src/Pages/Dashboard/Vehicle/styles.ts
@@ -1,11 +1,42 @@
 import styled from 'styled-components/native';
 import {View, Text} from 'react-native';
+import RNSpeedometer from 'react-native-speedometer';
 
 export const Container = styled.View`
-  flex: 1;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 100%;
+  background-color: #fefefe;
+  margin: 1% 1% 1% 0%;
+  border-radius: 10px;
+`;
+
+export const SpeedometerContainer = styled.View`
+  width: 70%;
+  justify-content: center;
+`;
+
+export const BlinkersContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const InfosContainer = styled.View`
+  flex-direction: column;
+  width: 30%;
+  padding: 2px;
+`;
+
+export const BatteryContainer = styled.View`
+  height: 50%;
   align-items: center;
   justify-content: center;
-  background-color: #fefefe;
-  margin: 5px 5px 5px 0;
-  border-radius: 10px;
+`;
+
+export const WeightContainer = styled.View`
+  height: 50%;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
 `;
