25dba6c36cbf7c5ec7f3557e0d71cf58d4dd17d4
Victor Rodrigues <victorrsilva17@gmail.com>
2021-09-29 20:17:05 -0300

    #4 Coletando localização do usuário
    Co-Authored-By: André Lucas <andre.lucas.lemos@hotmail.com>

--- ColetaFacil/src/Pages/MyMap/index.tsx
+++ ColetaFacil/src/Pages/MyMap/index.tsx
@@ -1,23 +1,42 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Button} from 'react-native-elements';
 import MapView from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
 
 import MenuBar from '../../Components/MenuBar';
 import {Container, ContentContainer} from './styles';
+import {colorsDark} from 'react-native-elements/dist/config';
 // AIzaSyDRUZ_X0Tuvhr9QaGabZBAwfRa3lJXhTcI
+
 const Report: React.FC = () => {
+  const [origin, setOrigin] = useState(null);
+  const [destination, setDestination] = useState(null);
+
+  useEffect(() => {
+    (function () {
+      Geolocation.getCurrentPosition(
+        location => {
+          console.log(location);
+          setOrigin({
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.00922,
+            longitudeDelta: 0.00421,
+          });
+        },
+        error => console.log(error),
+        {enableHighAccuracy: true},
+      );
+      console.log('Localização: ', origin);
+    })();
+  }, []);
   return (
     <Container>
       <MenuBar />
       <ContentContainer>
         <MapView
           style={{width: '100%', height: '100%'}}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
+          initialRegion={origin}
         />
       </ContentContainer>
     </Container>
