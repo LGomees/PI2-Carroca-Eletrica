c3899218b12710fe7066f25de11c56758ed0e467
Victor Rodrigues <victorrsilva17@gmail.com>
2021-10-18 20:27:56 -0300

    Configurando text inputs

--- ColetaFacil/src/Pages/Register/index.tsx
+++ ColetaFacil/src/Pages/Register/index.tsx
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {ButtonText, Container, Input, RegisterButton, Title} from './styles';
 import {useNavigation} from '@react-navigation/native';
 
 const Register: React.FC = () => {
   const navigation = useNavigation();
 
+  const [user, setUser] = useState(null);
+
+  const registerUser = async dados => {};
+
   return (
     <Container>
       <Title>CADASTRO</Title>
-      <Input placeholder={'Número da Carroça'} />
-      <Input placeholder={'Email'} />
-      <Input placeholder={'Senha'} />
-      <Input placeholder={'Confirmação de Senha'} />
+      <Input
+        placeholder={'Número da Carroça'}
+        placeholderTextColor={'black'}
+        keyboardType={'numeric'}
+        returnKeyLabel={'next'}
+        autoCorrect={false}
+      />
+      <Input
+        placeholder={'Email'}
+        placeholderTextColor={'black'}
+        keyboardType={'email-address'}
+        autoCapitalize={'none'}
+        returnKeyLabel={'next'}
+      />
+      <Input
+        placeholder={'Senha'}
+        placeholderTextColor={'black'}
+        autoCapitalize={'none'}
+        returnKeyLabel={'next'}
+        secureTextEntry
+        autoCorrect={false}
+      />
+      <Input
+        placeholder={'Confirmação de Senha'}
+        placeholderTextColor={'black'}
+        autoCapitalize={'none'}
+        returnKeyLabel={'done'}
+        secureTextEntry
+        autoCorrect={false}
+      />
       <RegisterButton onPress={() => navigation.navigate('Dashboard')}>
         <ButtonText>CADASTRAR</ButtonText>
       </RegisterButton>
